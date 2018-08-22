322e415d5e12a3491a1f98808cbdd8a5e069cc07
bireckia <Birecki.A@eplan.pl>
2018-08-22 14:37:45 +0200

    init commit

---
+++ Algorithms_tests/Algorithms_tests/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms_tests
+{
+    public static class Program
+    {
+        static void UnbalancedBSTSort<T>(this List<T> collection) where T: IComparable<T>
+        {
+            if (collection.Count == 0 )
+            {
+                return;
+            }
+            Node
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Algorithms_tests/BSTS_Models/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.IComparable;
+
+namespace BSTS_Models
+{
+    public class Node<T>: IComparable<Node<T>> where T: IComparable<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Parent { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node()
+        {
+            Value = default(T);
+            Parent = null;
+            Left = null;
+            Right = null;
+        }
+
+        public int CompareTo(Node<T> other)
+        {
+            if (other == null) return -1;
+            return this.Value.CompareTo(other.Value);
+        }
+    }
+}
