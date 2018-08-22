85f2da74cb98506f360b4d42305c1b9695ad0a29
Arnulf <abirecki@gmail.com>
2018-08-22 21:06:35 +0200

    refactoring

--- Algorithms_tests/Algorithms_tests/Program.cs
+++ Algorithms_tests/Algorithms_tests/Program.cs
@@ -1,19 +1,19 @@
-﻿using System;
+﻿using BSTS_Models;
+using System;
 using System.Collections.Generic;
 
 namespace Algorithms_tests
 {
-    public static class Program
+    public static class BinaryTreeSearchSorter
     {
-        static void UnbalancedBSTSort<T>(this List<T> collection) where T: IComparable<T>
+        public static void UnbalancedBSTSort<T>(this List<T> collection) where T: IComparable<T>
         {
             if (collection.Count == 0 )
-            {
                 return;
-            }
-            Node
 
-            Console.WriteLine("Hello World!");
+            Node<T> rootNode = new Node<T> { Value = collection[0]};
+
+
         }
     }
 }
